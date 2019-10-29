ee7a657df4debedeba9dab5fb2825a4dbbfb3961
adscript <adnanocode@gmail.com>
2019-10-30 01:51:59 +0700

    add redux, react-redux, and redux-thunk

--- client-react/src/index.js
+++ client-react/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Main from './components/Main';
 import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import contactReducer from './reducers';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(contactReducer, applyMiddleware(thunk));
+ 
+
+ReactDOM.render(<Provider store={store}><Main /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
